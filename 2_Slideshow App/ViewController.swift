40815ea4928861b3f9939a02ea327ea8c062cfcf
ryoko.hoshi <ryoko.hoshi@gmail.com>
2022-05-10 07:02:26 +0900

    Initial Commit

---
+++ 2_Slideshow App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_Slideshow App
+//
+//  Created by 星涼子 on 2022/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
